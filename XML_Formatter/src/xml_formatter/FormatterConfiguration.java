082bc5ab0503d201b4acfbc9d27433e60d6c527e
Michael Dietrich <michael_1276@yahoo.de>
2013-10-13 13:55:55 +0200

    added some more configuration settings

--- XML_Formatter/src/xml_formatter/FormatterConfiguration.java
+++ XML_Formatter/src/xml_formatter/FormatterConfiguration.java
@@ -1,7 +1,15 @@
 package xml_formatter;
 
-public class FormatterConfiguration {
-  public char indentation_character      = ' ';
-  public int  indentation_size           = 2;
-  public int  attribute_indentation_size = 2;
+public final class FormatterConfiguration {
+  
+  public final static int DO_NOT_CHANGE = 0;
+  public final static int SINGLE_LINE   = 1;
+  public final static int INDENT_ALL    = 2;
+  
+  public char    indentation_character      = ' ';
+  public int     indentation_size           = 2;
+  public int     attribute_indentation_size = 2;
+  public int     attribute_formatting       = DO_NOT_CHANGE;
+  public boolean keep_text_together         = true;
+  public boolean keep_empty_lines           = true;
 }
